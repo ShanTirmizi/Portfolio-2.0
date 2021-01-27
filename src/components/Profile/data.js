4fd52ef9cf138db7591bd781d3a5bf1989c93088
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2021-01-27 17:28:32 +0000

    Added the movies

---
+++ src/components/Profile/data.js
@@ -0,0 +1,124 @@
+const data = [
+    {
+        id: 1,
+        image: 'images/Stranger-Things.png',
+        name: 'Stranger Things',
+        rating: '8.7'
+    },
+    {
+        id: 2,
+        image: 'images/Black-Mirror-2.png',
+        name: 'Black Mirror',
+        rating: '8.8'
+    },
+    {
+        id: 3,
+        image: 'images/Daredevil.jpg',
+        name: 'Daredevil',
+        rating: '8.6'
+    },
+    {
+        id: 4,
+        image: 'images/Narcos.png',
+        name: 'Narcos',
+        rating: '8.8'
+    },
+    {
+        id: 5,
+        image: 'images/Peaky-Blinders.jpg',
+        name: 'Peaky Blinders',
+        rating: '8.8'
+    },
+    {
+        id: 6,
+        image: 'images/Money-Heist.png',
+        name: 'Money Heist',
+        rating: '8.3'
+    },
+    {
+        id: 7,
+        image: 'images/Dark.jpg',
+        name: 'Dark',
+        rating: '8.8'
+    },
+    {
+        id: 8,
+        image: 'images/Arrested-Development.jpg',
+        name: 'Arrested Development',
+        rating: '8.7'
+    },
+    {
+        id: 9,
+        image: "images/The-Queen's-Gambit.jpg",
+        name: "The Queen's Gambit",
+        rating: '8.6'
+    },
+    {
+        id: 10,
+        image: 'images/mindhunter.jpg',
+        name: 'Mindhunter',
+        rating: '8.6'
+    },
+    {
+        id: 11,
+        image: 'images/Ozark.jpg',
+        name: 'Ozark',
+        rating: '8.4'
+    },
+    {
+        id: 12,
+        image: 'images/The-Umbrella-Academy.jpg',
+        name: 'The Umbrella Academy',
+        rating: '8.0'
+    },
+    {
+        id: 13,
+        image: 'images/The-Crown-season.jpeg',
+        name: 'The Crown',
+        rating: '8.7'
+    },
+    {
+        id: 14,
+        image: 'images/You.png',
+        name: 'You',
+        rating: '7.7'
+    },
+    {
+        id: 15,
+        image: 'images/Altered-Carbon.jpg',
+        name: 'Altered Carbon',
+        rating: '8.0'
+    },
+    {
+        id: 16,
+        image: 'images/BoJack-Horseman.jpg',
+        name: 'BoJack Horseman',
+        rating: '8.7'
+    },
+    {
+        id: 17,
+        image: 'images/cobra-kia.jpeg',
+        name: 'Cobra Kai',
+        rating: '8.6'
+    },
+    {
+        id: 18,
+        image: 'images/Love-Death+Robots.png',
+        name: 'Love, Death & Robots',
+        rating: '8.5'
+    },
+    {
+        id: 19,
+        image: 'images/When-they-see-us.jpg',
+        name: 'When They See Us',
+        rating: '8.9'
+    },
+    {
+        id: 20,
+        image: 'images/AFTER-LIFE.jpg',
+        name: 'After Life',
+        rating: '8.4'
+    }
+]
+
+export { data }
