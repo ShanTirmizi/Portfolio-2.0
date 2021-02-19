b7069e55507318abee265aa891a3b421b2ba6165
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2021-02-19 18:29:48 +0000

    added project detail

--- src/components/Projects/ProjectData.js
+++ src/components/Projects/ProjectData.js
@@ -22,7 +22,7 @@ export const ProjectData = [
         video: Covid,
         Site: 'https://shan-covid-tracker.netlify.app/',
         GitHub: 'https://github.com/ShanTirmizi/Covid-19-Tracker',
-        description: 'A Covid Tracker created using React. The app shows the number of cases, recovered & deaths on a map created using Leaflet js and on charts created using Chart js. The app also shows the list of countries in a table from the highest infections to the lowest. The data is fetched from the disease.sh API.'
+        description: "A Covid Tracker created using React. The app shows the number of cases, recovered & deaths on a map created using Leaflet js and on charts created using Chart js. The app also shows the list of countries in a table from the highest infections to the lowest. The data is fetched from an API from 'disease.sh'."
     },
     {
         title: 'E-commerce',
@@ -36,7 +36,7 @@ export const ProjectData = [
         video: Book,
         Site: 'https://airbnb-andreeaionescu97.herokuapp.com/',
         GitHub: 'https://github.com/andreeaionescu97/rails-textbook-exchange',
-        description: "An eBay like book exchange created using Ruby on Rails where users can lend their used books and other users on the website can rent them for a specified period. The users can also rate the book and accept or decline a rent request. This apps also has user authentication built-in."
+        description: "An eBay like book exchange created using Ruby on Rails where users can lend their used books and other users on the website can rent them for a specified period. Users can also rate the book and accept or decline a rent request. This apps also has user authentication built-in."
 
     },
     {
@@ -44,14 +44,14 @@ export const ProjectData = [
         video: GitHubUser,
         Site: 'https://shan-github-search.netlify.app/',
         GitHub: 'https://github.com/ShanTirmizi/GitHub-User-Search',
-        description: 'A GitHub Search app created using React. The app allows you to search GitHub users and gives you their information fetched from the GitHub API accompanied by four charts.'
+        description: "A GitHub Search app created using React. Users can search for GitHub users and it will fetch the data from GitHub's API and display their info accompanied by four charts created using Fusion Charts."
     },
     {
         title: 'Crypto Tracker',
         video: Crypto,
         Site: 'https://shan-crypto-tracker.netlify.app/',
         GitHub: 'https://github.com/ShanTirmizi/Crypto-Price-Tracker',
-        description: 'A colourful Crypto Tracker built using React. It shows the top 100 cryptocurrencies displayed in a list form on the home page and the Coin page containing further information about each currency. The data is fetched from the Coingecko API.'
+        description: "A colourful Crypto Tracker built using React. It shows the top 100 cryptocurrencies displayed in a list form on the home page and the Coin page containing further information about each currency. The data is fetched from Coingecko's API."
     },
     {
         title: 'Energy Management',
